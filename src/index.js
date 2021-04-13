5314a4361e5a6b8062c4f6787f2110fea2529d5c
Jackie Ye <jackieye27@gmail.com>
2021-04-13 15:57:38 -0700

    hard coded components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const firstBook = {
+  img:'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  title: 'Title1',
+  author: 'Author1'
+}
+const secondBook = {
+  img:'https://images-na.ssl-images-amazon.com/images/I/91dylEq7LML._AC_UL200_SR200,200_.jpg',
+  title: 'Title2',
+  author: 'Author2'
+}
+
+const BookList = () => {
+  return (
+  <section className='bookList'>
+    <Book img = {firstBook.img} title = {firstBook.title} author = {firstBook.author}> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit adipisci accusantium, a quas ad dicta aliquid libero eius quae veniam!</p>
+    </Book>
+    <Book img = {secondBook.img} title = {secondBook.title} author = {secondBook.author}/>
+  </section>
+  )
+}
+
+const Book = (props) => {
+  const { img, title, author, children} = props
+  console.log(props);
+  return (
+    <article className='book'>
+      <img src={img} alt=""/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  )
+}
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
